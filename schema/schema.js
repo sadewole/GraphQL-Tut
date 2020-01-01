091859e57c83f164f3df738ba10f6d824b5602bd
Samuel Adewole <samueladewole15@gmail.com>
2020-01-01 17:36:04 +0100

    connect model to schema

--- schema/schema.js
+++ schema/schema.js
@@ -29,9 +29,11 @@ const BookType = new GraphQLObjectType({
         author: {
             type: AuthorType,
             resolve(parent, args) {
-                return _.find(authors, {
-                    id: parent.authorId
-                })
+                // return _.find(authors, {
+                //     id: parent.authorId
+                // })
+
+                return Author.findById(parent.authorId)
             }
         }
     })
@@ -52,7 +54,11 @@ const AuthorType = new GraphQLObjectType({
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args) {
-                return _.filter(books, {
+                // return _.filter(books, {
+                //     authorId: parent.id
+                // })
+
+                return Book.find({
                     authorId: parent.id
                 })
             }
@@ -72,9 +78,10 @@ const RootQuery = new GraphQLObjectType({
             },
             resolve(parent, args) {
                 // code to get data from db/ other source
-                return _.find(books, {
-                    id: args.id
-                })
+                // return _.find(books, {
+                //     id: args.id
+                // })
+                return Book.findById(args.id)
             }
         },
         author: {
@@ -85,21 +92,24 @@ const RootQuery = new GraphQLObjectType({
                 }
             },
             resolve(parent, args) {
-                return _.find(authors, {
-                    id: args.id
-                })
+                // return _.find(authors, {
+                //     id: args.id
+                // })
+                return Author.findById(args.id)
             }
         },
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args) {
-                return books
+                // return books
+                return Book.find({})
             }
         },
         authors: {
             type: new GraphQLList(AuthorType),
             resolve(parent, args) {
-                return authors
+                // return authors
+                return Author.find({})
             }
         }
     }
